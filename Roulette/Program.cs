9e1f4377feefe2688a08463fa15e7c7b372cf468
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-09-28 11:21:00 -0400

    adds some comments but more importantly moves the arrays to outside of the main method

--- Roulette/Program.cs
+++ Roulette/Program.cs
@@ -6,18 +6,19 @@ using System.Threading.Tasks;
 
 namespace Roulette
 {
+    // enum for colors so that the colors have bounds and are easier to work with
     enum Colors { Black, Red, Green};
 
     class Program
     {
-        static void Main(string[] args)
-        {
-            int[] wheelNums = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+        // this array is all the numbers on a roulette wheel in numerical order
+        int[] wheelNums = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
                                           10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
                                           20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
                                           30, 31, 32, 33, 34, 35, 36, 00  };
 
-            Colors[] wheelColors = new Colors[] { Colors.Green, Colors.Red, Colors.Black, Colors.Red, Colors.Black, Colors.Red,
+        // this array is the colors on the wheel and it matches the number array
+        Colors[] wheelColors = new Colors[] { Colors.Green, Colors.Red, Colors.Black, Colors.Red, Colors.Black, Colors.Red,
                                                 Colors.Black, Colors.Red, Colors.Black, Colors.Red, Colors.Black,
                                                 Colors.Black, Colors.Red, Colors.Black, Colors.Red, Colors.Black,
                                                 Colors.Red, Colors.Black, Colors.Red, Colors.Red, Colors.Black,
@@ -25,6 +26,10 @@ namespace Roulette
                                                 Colors.Black, Colors.Red, Colors.Black, Colors.Black, Colors.Red,
                                                 Colors.Black, Colors.Red, Colors.Black, Colors.Red, Colors.Black,
                                                 Colors.Red, Colors.Green };
+
+        static void Main(string[] args)
+        {
+            
         }
     }
 }
