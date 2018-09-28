c115c8186b5fb5e9bb71e2dba69b5d46eef0f276
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-09-28 11:17:27 -0400

    creates an array for the colors on the wheel

--- Roulette/Program.cs
+++ Roulette/Program.cs
@@ -17,6 +17,14 @@ namespace Roulette
                                           20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
                                           30, 31, 32, 33, 34, 35, 36, 00  };
 
+            Colors[] wheelColors = new Colors[] { Colors.Green, Colors.Red, Colors.Black, Colors.Red, Colors.Black, Colors.Red,
+                                                Colors.Black, Colors.Red, Colors.Black, Colors.Red, Colors.Black,
+                                                Colors.Black, Colors.Red, Colors.Black, Colors.Red, Colors.Black,
+                                                Colors.Red, Colors.Black, Colors.Red, Colors.Red, Colors.Black,
+                                                Colors.Red, Colors.Black, Colors.Red, Colors.Black, Colors.Red,
+                                                Colors.Black, Colors.Red, Colors.Black, Colors.Black, Colors.Red,
+                                                Colors.Black, Colors.Red, Colors.Black, Colors.Red, Colors.Black,
+                                                Colors.Red, Colors.Green };
         }
     }
 }
