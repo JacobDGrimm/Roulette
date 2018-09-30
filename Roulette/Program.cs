4419f4ea3f9fbb5fd9c5a5fa3c4d8bc02cfd1fd5
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-09-29 21:11:13 -0400

    moved the arrays into a class for the table elements

--- Roulette/Program.cs
+++ Roulette/Program.cs
@@ -11,21 +11,7 @@ namespace Roulette
 
     class Program
     {
-        // this array is all the numbers on a roulette wheel in numerical order
-        int[] wheelNums = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-                                          10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-                                          20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-                                          30, 31, 32, 33, 34, 35, 36, 00  };
-
-        // this array is the colors on the wheel and it matches the number array
-        Colors[] wheelColors = new Colors[] { Colors.Green, Colors.Red, Colors.Black, Colors.Red, Colors.Black, Colors.Red,
-                                                Colors.Black, Colors.Red, Colors.Black, Colors.Red, Colors.Black,
-                                                Colors.Black, Colors.Red, Colors.Black, Colors.Red, Colors.Black,
-                                                Colors.Red, Colors.Black, Colors.Red, Colors.Red, Colors.Black,
-                                                Colors.Red, Colors.Black, Colors.Red, Colors.Black, Colors.Red,
-                                                Colors.Black, Colors.Red, Colors.Black, Colors.Black, Colors.Red,
-                                                Colors.Black, Colors.Red, Colors.Black, Colors.Red, Colors.Black,
-                                                Colors.Red, Colors.Green };
+        
 
         static void Main(string[] args)
         {
