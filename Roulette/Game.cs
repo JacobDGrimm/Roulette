9edd9a2c02645bab1a7e6ca2e3d41b00c7a66177
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-09-29 22:47:10 -0400

    adds number even red and high bet results

--- Roulette/Game.cs
+++ Roulette/Game.cs
@@ -8,17 +8,54 @@ namespace Roulette
 {
     class Game
     {
+        Table tab = new Table();
+
+
         public void SpinTheBallGame()
         {
-            Table tab = new Table();
-
             tab.Spin();
             Console.WriteLine($"The winning number is: {tab.ColorResult()} {tab.NumResult()}");
         }
 
         public void WinningBets()
         {
+            
+            if ( tab.NumResult() == 0 || tab.NumResult() == 00 )
+            {
+                Console.WriteLine($"Only {tab.ColorResult()} {tab.NumResult()} gets paid this time!");
+            }
+            else
+            {
 
+            }
         }
+
+        public void NumberBet()
+        {
+            Console.WriteLine($"Everyone who bet on {tab.NumResult()} wins.");
+        }
+
+        public void EvenOddBet()
+        {
+            if ( tab.NumResult() % 2 == 0 )
+                Console.WriteLine($"Everyone who bet even wins.");
+            if ( tab.NumResult() % 2 != 0 )
+                Console.WriteLine("Everyone who bet odd wins.");
+        }
+
+        public void RedBlackBet()
+        {
+            Console.WriteLine($"Everyone who bet on {tab.ColorResult()} wins.");
+        }
+
+        public void HighLowBet()
+        {
+            if ( tab.NumResult() <= 18 )
+                Console.WriteLine("Everyone who bet on \"1 to 18\" wins.");
+            if ( tab.NumResult() > 18 )
+                Console.WriteLine("Everyone who bet on \"19 to 36\" wins.");
+        }
+
+
     }
 }
